91d52fac03d36f8bdbc0214edbf811250bbe3de8
zhanghao <996215406@qq.com>
2021-08-29 13:43:02 +0800

    添加项目文件。

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+		double a;
+		char b;
+		int c;
+		char iden;
+		cin >> a;
+		cin.get(b);
+		cin >> c;
+		map<char, double> table{ {'T',1e12},{'G',1e9}, {'M',1e6},{'k',1e3},
+			{'m',1e-3},{'u',1e-6},{'n',1e-9},{'p',1e-12},{'f',1e-15},{'a',1e-18}};
+		for (auto i = table.begin(); i != table.end(); ++i)
+		{
+			if ((a / (i->second) >= 1) && (a / (i->second) < 1000))
+			{
+				iden = i->first;
+			}
+		}
+		double result = a / table[iden];
+		cout << setprecision(c) << result<< iden << endl;
+		return 0;
+
+}
+
